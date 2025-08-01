ccbbdba3804487eaa1d99388dff8d9e7d246f985
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-08-01 10:19:13 +0200

    commit

--- src/main/java/com/leclowndu93150/apothic_patches/ApothicPatches.java
+++ src/main/java/com/leclowndu93150/apothic_patches/ApothicPatches.java
@@ -2,14 +2,10 @@ package com.leclowndu93150.apothic_patches;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerAboutToStartEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
